3c8edaf0d2acffb185bb4e9b5bacc72d6c172b3a
Cristiano616 <juniorcristianobr@gmail.com>
2022-03-26 15:19:38 -0300

    finalizar exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Esperar o usuario pressionar uma tecla...
+Console.ReadKey();
+// Exibir a palavra preparar...
+Console.WriteLine("preparar...");
+// Esperar o usuario pressionar uma tecla...
+Console.ReadKey();
+// Exibir a palavra apontar...
+Console.WriteLine("apontar...");
+// Espera o usuario pressionar uma tecla para finalizar
+Console.ReadKey();
+// Exiba a palavra FOGO!
+Console.Write("FOGO!");
+Console.ReadKey();
+
